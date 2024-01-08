b3e98cc6f50d4c61c214b0e475426e0de8bbc5c1
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2024-01-08 23:20:49 +0700

    cài đặt cors

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, {Express, Request, Response} from "express"
 import dotenv from "dotenv";
 import * as database from "./config/database";
-import Task from "./api/v1/models/task.model";
+import cors from 'cors'
 import  routeApiVer1  from "./api/v1/routes/index.route";
 import bodyParser from 'body-parser'
 const app: Express = express();
@@ -9,7 +9,7 @@ const port: number | string = process.env.PORT || 3000
 dotenv.config();
 database.connect();
 app.use(bodyParser.json())
-
+app.use(cors());
 routeApiVer1(app);
 
 app.listen(port, () => {
