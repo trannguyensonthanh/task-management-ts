afe900dffbac364ceb9e91d845ba7bf6c8f97714
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2024-01-08 14:51:07 +0700

    đổi trạng thái nhiều công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -74,3 +74,39 @@ export const changeStatus = async (req: Request, res: Response) => {
   }
 
 };
+
+export const changeMulti = async (req: Request, res: Response) => {
+  try {
+    const ids: string[] = req.body.ids;
+    const key: string = req.body.key;
+    const value: string = req.body.value;
+    switch(key){
+       case "status":
+    await Task.updateMany({
+      _id: {$in: ids}
+    }, {
+      status: value
+    })
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công"
+     })
+     break;
+     
+     default:
+      res.json({
+        code: 400,
+        message: "Cập nhật trạng thái thất bại"
+       })
+    }
+     
+
+  
+  } catch (error){
+  res.json({
+    code: 400,
+    message: "Lỗi"
+  })
+  }
+
+};
--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -5,6 +5,7 @@ import * as controller from "../controllers/task.controller";
 router.get("/", controller.index);
 router.get("/detail/:id", controller.taskDetail);
 router.patch("/change-status/:id", controller.changeStatus);
+router.patch("/change-multi", controller.changeMulti);
 
 
 export const taskRoutes: Router = router;
